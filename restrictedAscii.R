fddf96b124ed2af2d3c5937bfce9a1c3251a8369
Callum <25870391+CDuncan@users.noreply.github.com>
2019-09-26 01:54:44 +0100

    mapMerge input change
    Changed mapMerge input from codified filters to accepting list of cells to merge

--- restrictedAscii.R
+++ restrictedAscii.R
@@ -2,28 +2,20 @@ library(rgdal)
 library(raster) 
 library(tidyverse)
 
-# make a list of file names, perhaps like this:  
-f <-list.files(path="asc/Extracted",pattern = ".asc",full.names = T)  
+selectedCells <- 
+  read_csv("asc/selectedCells.csv") %>%
+  pull(cellName) %>%
+  paste(.,collapse = '|')
 
-f <- f %>% 
+# make a list of file names, perhaps like this:  
+f <- 
+  list.files(path="asc/Extracted",pattern = ".asc",full.names = T) %>% 
   as_tibble() %>%
-  mutate(
-    cell= case_when(
-      str_detect(value,"NY") ~ "Max",
-      TRUE ~ "Min"),
-    northing=as.numeric(str_sub(value,-5,-5)),
-    easting=as.numeric(str_sub(value,-6,-6)))%>%
-  filter((northing <=5 & cell=="Max")|(northing >=6 & cell=="Min")) %>%
-  filter(easting <=5) %>%
+  filter(str_detect(value,selectedCells)) %>%
   pull(value)
 
 # turn these into a list of RasterLayer objects  
-r <- lapply(f, raster) 
-
-
-# as you have the arguments as a list call 'merge' with 'do.call'  
-x <- do.call("merge",r) 
-
-
-#Write Ascii Grid  
-writeRaster(x,"asc/mapMerge.asc")  
+r <- 
+  lapply(f, raster) %>%
+  do.call("merge",.) %>%  # as you have the arguments as a list call 'merge' with 'do.call'  
+  writeRaster("asc/mapMerge.asc")  #Write Ascii Grid  
